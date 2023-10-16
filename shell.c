3627dd122fd3ad41d3863f379070f707253a20bb
JoyceNjoro1 <Njorogejoyce60@gmail.com>
2023-10-16 03:14:52 -0700

    cd

--- shell.c
+++ shell.c
@@ -2,45 +2,41 @@
 #include "string.h"
 
 /**
- * cd_shell - Change the current directory based on input
+ * cd_shell - Change the current directory based on input.
+ * @datash: A pointer to data relevant to the shell.
  *
- * @datash: A pointer to data relevant to the shell
- * Return: 1 on success
+ * Return: 1 on success.
  */
 int cd_shell(data_shell *datash)
 {
-	char *target_dir;
-	int is_home, is_home2, is_ddash;
+    char *target_dir;
+    int is_home, is_home2, is_ddash;
 
-	target_dir = datash->args[1];
+    target_dir = datash->args[1];
 
-	if (target_dir != NULL)
-	{
-		is_home = strcmp("$HOME", target_dir);
-		is_home2 = strcmp("~", target_dir);
-		is_ddas =_strcmp("--", target_dir);
-	}
+    if (target_dir != NULL)
+    {
+        is_home = strcmp("$HOME", target_dir);
+        is_home2 = strcmp("~", target_dir);
+        is_ddash = strcmp("--", target_dir);
+    }
 
-	if (target_dir == NULL || !is_home || !is_home2 || !is_ddash)
-	{
-		cd_to_home(datash);
-		return (1);
-	}
+    if (target_dir == NULL || is_home == 0 || is_home2 == 0 || is_ddash == 0)
+    {
+        cd_to_home(datash);
+        return (1);
+    }
 
-	if (_strcmp("-", target_dir) == 0)
-	{
-		cd_previous(datash);
-		return (1);
-	}
+    if (strcmp("-", target_dir) == 0)
+    {
+        cd_previous(datash);
+        return (1);
+    }
 
-	if (_strcmp(".", target_dir) == 0 || _strcmp("..", target_dir) == 0)
-	{
-		cd_dot(datash);
-		return (1);
-	}
+    if (strcmp(".", target_dir) == 0 || strcmp("..", target_dir) == 0)
+    {
+    }
 
-	cd_to(datash);
-
-	return (1);
+    return (1);
 }
 
