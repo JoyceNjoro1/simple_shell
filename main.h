e7f45dc42826ce18968c4676bf2eb7df50f1af22
Shedie254 <shadrackmuchiri43@gmail.com>
2023-10-17 11:18:36 -0700

    shell

--- main.h
+++ main.h
@@ -106,5 +106,8 @@ char *strcat_cd(data_shell *datash, char *msg, char *error, char *line_count);
 char *error_get_cd(data_shell *datash);
 char *error_not_found(data_shell *datash);
 char *error_exit_shell(data_shell *datash);
+void _memcpy(void *newptr, const void *ptr, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
